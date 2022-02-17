3190e018f6d6e226e145669ec9a5857c2e41f49f
awesome310798 <itspmk-70@mail.ru>
2022-02-17 13:03:06 +0300

    My HomeWork

---
+++ src/main/java/ru/geekbrains/home1/HomeWorkApp.java
@@ -0,0 +1,49 @@
+package ru.geekbrains.home1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = -2;
+        int b = 1;
+        int c = a + b;
+        if(c>= 0){
+            System.out.println("Сумма положительная");
+        }else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor(){
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value >= 1 && value <= 100 ) {
+            System.out.println("желтый");
+        }
+        else if (value > 100){
+            System.out.println("Зеленый");
+        }
+
+    }
+    public static void compareNumbers(){
+        int a = 2;
+        int b = 3;
+        if (a >= b){
+            System.out.println("a >= b");
+        }else {
+            System.out.println("a < b");
+        }
+    }
+
+}
